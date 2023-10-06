58e0d17b6eacf62e1d4782b0e4e58ad39e1cddfa
emsi06001 <emsi06001@E-PW0234N3>
2023-10-06 10:14:53 +0200

    Add project files.

---
+++ inluppg3.7/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Xml;
+
+namespace inluppg3_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in en beräkning");
+            string input = Console.ReadLine();
+
+            if (input.Contains('-'))
+            {
+                int left = int.Parse(input[..input.IndexOf('-')]);
+                int right = int.Parse(input[(input.IndexOf('-')+1)..]);
+                int output = left - right;
+                Console.WriteLine(input + " = " +output);
+            }
+            else if (input.Contains('+'))
+            {
+                int left = int.Parse(input[..input.IndexOf('+')]);
+                int right = int.Parse(input[(input.IndexOf('+') + 1)..]);
+                int output = left + right;
+                Console.WriteLine(input + " = " + output);
+            }
+            else
+            {
+                Console.WriteLine("Fel inmatning");
+            }
+        }
+    }
+}
